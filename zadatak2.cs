edb307d528f60f8e63124016093624de613ca333
gogi150 <153612601+gogi150@users.noreply.github.com>
2023-12-12 13:01:55 +0100

    Update zadatak2.cs

--- zadatak2.cs
+++ zadatak2.cs
@@ -6,7 +6,7 @@ public class Program
 		int b = 7;
 		double c = 12.5;
 		double d = 16.7;
-		double x = a+b+c+d/4;
+		double x = (a+b+c+d)/4;
 		Console.WriteLine("Prosjecna vrijednost je: " + x);
 	}
 }
