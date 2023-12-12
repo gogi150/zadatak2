68414d45a9de999d6e4d68abf25a54ff6041b085
gogi150 <153612601+gogi150@users.noreply.github.com>
2023-12-12 12:59:24 +0100

    Add files via upload

---
+++ zadatak2.cs
@@ -0,0 +1,12 @@
+public class Program
+{
+	public static void Main()
+	{
+		int a = 10;
+		int b = 7;
+		double c = 12.5;
+		double d = 16.7;
+		double x = a+b+c+d/4;
+		Console.WriteLine("Prosjecna vrijednost je: " + x);
+	}
+}
